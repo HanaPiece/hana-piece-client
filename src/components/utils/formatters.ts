4306c062ac34fcf848b0f6286717cc972766717e
Seoha Lee <seohalee0131@gmail.com>
2024-05-29 20:36:27 +0900

    design: header 아이콘으로 변경 및 현재 시간 보여주기

--- src/components/utils/formatters.ts
+++ src/components/utils/formatters.ts
@@ -1,3 +1,16 @@
 export const addCommas = (num: number): string => {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
+
+export const currentTime = (): string => {
+  const date = new Date();
+  let hours = date.getHours();
+  const minutes = date.getMinutes();
+
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+
+  return `${hours}:${formattedMinutes}`;
+}
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { NavBar } from "./common/NavBar";
+import { Header } from "./common/Header";
 
 export const Layout = () => {
   return (
     <>
       <div className="container flex flex-col relative">
+        <Header />
         <div className="flex-grow overflow-auto mb-20">
           <Outlet />
         </div>
