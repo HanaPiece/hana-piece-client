72e909b6909d1264155dce8264fbf00c20a55eed
Seoha Lee <seohalee0131@gmail.com>
2024-06-04 11:20:43 +0900

    feat: date 문자열 파싱 추가

--- src/components/utils/formatters.ts
+++ src/components/utils/formatters.ts
@@ -15,11 +15,19 @@ export const currentTime = (): string => {
   return `${hours}:${formattedMinutes}`;
 }
 
+// 'YYYY-MM-DD' to 'YYYY'
+export const getYearFromDateString = (dateString: string): string => {
+  const parts = dateString.split("-");
+  return parts[0];
+}
+
+// 'YYYY-MM-DD' to 'MM'
 export const getMonthFromDateString = (dateString: string): string => {
   const parts = dateString.split("-");
   return parts[1];
 }
 
+// 'YYYY-MM-DD' to 'YYYY년 MM월 DD일 W요일'
 export const dateParse = (date: string): string => {
   const dateObject = new Date(date);
   const year = dateObject.getFullYear();
@@ -31,4 +39,13 @@ export const dateParse = (date: string): string => {
   const dayName = daysOfWeek[dayOfWeek];
 
   return `${year}년 ${month}월 ${day}일 ${dayName}`;
+};
+
+// 'YYYYMMDD' to 'YYYY-MM-DD'
+export const goalDateParse = (date:string):string => {
+  const year = date.substring(0, 4);
+  const month = date.substring(4, 6);
+  const day = date.substring(6, 8);
+  
+  return `${year}-${month}-${day}`;
 };
